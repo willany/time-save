3531b9685def9832f00b52c7ad9fbbaab48c7287
wsrmelo <willany_lp@hotmail.com>
2018-07-05 17:12:08 -0300

    adicionando o rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
-    TopoComponent
+    TopoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
