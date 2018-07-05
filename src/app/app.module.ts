506a9ec6a61ce54a0471a65112c47c61fd339869
Haysa Rodrigues <hjsilva@thoughtworks.com>
2018-07-05 15:01:38 -0300

    Add componente de topo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { TopoComponent } from './topo/topo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule
