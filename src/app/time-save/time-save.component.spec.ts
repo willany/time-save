2875f1c47c47490cfa1c41758e70cdfab54eb978
wsrmelo <willany_lp@hotmail.com>
2018-08-06 16:34:42 -0300

    jenkins

--- src/app/time-save/time-save.component.spec.ts
+++ src/app/time-save/time-save.component.spec.ts
@@ -1,57 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { environment } from '../../environments/environment';
-import { AngularFireModule } from 'angularfire2';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from '../app.component';
 import { FormsModule } from '@angular/forms';
 import { TimeSaveComponent } from '../time-save/time-save.component';
 import { TimeListComponent } from '../time-list/time-list.component';
 import { RodapeComponent } from '../rodape/rodape.component';
 import { IdentityService } from '../shared/identity.service';
-import { EntryService } from '../shared/entry.service';
-
 import { AngularFireDatabase } from 'angularfire2/database';
 import { BehaviorSubject } from 'rxjs';
-
 describe('TimeSaveComponent', () => {
   let component: TimeSaveComponent;
   let fixture: ComponentFixture<TimeSaveComponent>;
-  // const FirestoreStub = {
-  //   collection: (name: string) => ({
-  //     doc: (_id: string) => ({
-  //       valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
-  //       set: (_d: any) => new Promise((resolve, _reject) => resolve()),
-  //     }),
-  //   }),
-  // };
-
+  const FirestoreStub = {
+    collection: (name: string) => ({
+      doc: (_id: string) => ({
+        valueChanges: () => new BehaviorSubject({ foo: 'bar' }),
+        set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+      }),
+    }),
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        AngularFireModule.initializeApp(environment.firebase, 'time-save'),
-        FormsModule
-       ],
+      imports: [FormsModule],
       declarations: [
         AppComponent,
         TimeSaveComponent,
         TimeListComponent,
         RodapeComponent
       ],
-      providers: [
-        EntryService,
-        IdentityService, { provide: AngularFireDatabase }
-      ]
+      providers: [IdentityService, { provide: AngularFireDatabase, useValue: FirestoreStub }]
     }).compileComponents();
   }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(TimeSaveComponent);
     component = fixture.componentInstance;
-    component.userId = 1234;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
+  it('should create', async(() => {
     expect(component).toBeTruthy();
-  });
+  }));
 });
